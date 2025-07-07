5e7178abfb7800772758563a07875176a9e35eff
Olga Fedotova <olyafedotova13@gmail.com>
2025-07-07 17:42:42 +0200

    Time: 325 ms (15.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT *,
+CASE WHEN (ABS(x) + ABS(y) > ABS(z))
+    AND (ABS(x) + ABS(z) > ABS(y))
+    AND (ABS(z) + ABS(y) > ABS(x))
+    THEN "Yes" ELSE "No" END AS triangle 
+FROM Triangle
